ea9e9df66b85297722545fc14fffe5ed33c49e3e
shaheer-khan-github <shaheerkhan36302@gmail.com>
2023-04-12 00:15:44 +0500

    Post_Get_FastAPI_Call_Docker

---
+++ fraud_voting_model_api_with_docker.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+from pycaret.classification import load_model, predict_model
+from fastapi import FastAPI
+import uvicorn
+
+# Create the app
+app = FastAPI()
+
+# Load trained Pipeline
+model = load_model('fraud_voting_model_api')
+
+# Define predict function
+@app.post('/predict')
+def predict(Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount):
+    data = pd.DataFrame([[Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount]])
+    data.columns = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
+    predictions = predict_model(model, data=data) 
+    return {'prediction': list(predictions['Label'])}
+
+@app.get('/get_predict')
+def predict(Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount):
+    data = pd.DataFrame([[Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount]])
+    data.columns = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
+    predictions = predict_model(model, data=data) 
+    return {'prediction': list(predictions['Label'])}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8001)
